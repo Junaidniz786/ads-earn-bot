109f6999368d9f7809fd8ff8ac8149977d2d1cde
Junaidniz786 <Junaid20305@gmail.com>
2025-11-05 16:03:30 +0500

    Fix duplicate os.environ.get calls in config.py

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "Y8398491491:AAFKnww5QjxKCgJ3DissBWT5IzHNdvpqYtM")
 # ADMIN_IDS as comma separated "7011937754"
 ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS","").split(",") if x.strip().isdigit()]
-CHANNEL_OR_GROUP = os.environ.get(CHANNEL_OR_GROUP = os.environ.get("CHANNEL_OR_GROUP", "@Junaidniz786")
+
+# Error 1: 'os.environ.get' do baar tha, ab theek hai.
+CHANNEL_OR_GROUP = os.environ.get("CHANNEL_OR_GROUP", "@Junaidniz786")
 
 DB_PATH = os.environ.get("DB_PATH","ads_earn.db")
 DATABASE_URL = os.environ.get("DATABASE_URL")  # Railway Postgres will set this if added
@@ -15,4 +17,5 @@ MIN_WITHDRAW_USDT = float(os.environ.get("MIN_WITHDRAW_USDT", "5.0"))
 WITHDRAW_CURRENCY = os.environ.get("WITHDRAW_CURRENCY", "USDT")
 
 # Admin panel key
-ADMIN_KEY = os.environ.get(ADMIN_KEY = os.environ.get("ADMIN_KEY", "Junaid123@")
+# Error 2: 'os.environ.get' do baar tha, ab theek hai.
+ADMIN_KEY = os.environ.get("ADMIN_KEY", "Junaid123@")
